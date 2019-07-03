f3594fda781c8d41feabe7a355880ad17c116e10
DianaG-M <aries_di@hotmail.com>
2019-07-03 14:28:17 -0500

    Crea el pipe rating para ordenar por rating y cambia el nombre del otro pipe a uno más específico

---
+++ src/app/pipe/name.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'orderName'
+})
+export class NamePipe implements PipeTransform {
+  transform(arr: any []): any {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length - 1 - i; j++) {
+          if (arr[j].name > arr[j + 1].name) {
+              [arr[j].name, arr[j + 1].name] = [arr[j + 1].name, arr[j].name];
+          }
+      }
+    }
+    console.log(arr);
+    return arr;
+  }
+}
--- src/app/pipe/rating.pipe.ts
+++ src/app/pipe/rating.pipe.ts
@@ -1,14 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'orderBy'
+  name: 'orderRating'
 })
-export class PipePipe implements PipeTransform {
+export class RatingPipe implements PipeTransform {
+
   transform(arr: any []): any {
     for (let i = 0; i < arr.length; i++) {
       for (let j = 0; j < arr.length - 1 - i; j++) {
-          if (arr[j].name > arr[j + 1].name) {
-              [arr[j].name, arr[j + 1].name] = [arr[j + 1].name, arr[j].name];
+          if (arr[j].rating > arr[j + 1].rating) {
+              [arr[j].rating, arr[j + 1].rating] = [arr[j + 1].rating, arr[j].rating];
           }
       }
     }
