0bdbbe587ba4703507e1eb701fc30356980c5e47
DianaG-M <aries_di@hotmail.com>
2019-07-03 14:33:47 -0500

    Agrega la ruta en la que muestra los restaurantes ordenados por rating

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AscNameComponent } from './components/asc-name/asc-name.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { CommonModule } from '@angular/common';
+import { RatingComponent } from './components/rating/rating.component';
 
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'restaurants', component: MenuComponent },
     { path: 'asc-AZ', component: AscNameComponent },
+    { path: 'asc-rating', component: RatingComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'home'},
 ];
 
